fd5b280d5fd11005d1085806cf7f01808b31603c
leon birch <145602846+leonmeep@users.noreply.github.com>
2024-03-22 14:55:52 +0000

    created language seeder - only does one

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DifficultySeeder::class
         ]);
+        $this->call([
+            LanguageSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/LanguageSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class LanguageSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        DB::table('languages')->insert([
+            'name' => Str::random(100),
+            'description' => Str::random(500),
+            'spoken_by' => rand(1, 700)
+        ]);
+    }
+}
