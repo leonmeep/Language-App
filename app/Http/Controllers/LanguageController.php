cfb2a2a90431195a4da5de374a66ac56517b1c50
leon birch <145602846+leonmeep@users.noreply.github.com>
2024-03-22 13:37:46 +0000

    created jsonResponseService

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\JsonResponseService;
 use Illuminate\Http\Request;
 
 class LanguageController extends Controller
 {
+    public function _construct(JsonResponseService $responseService)
+    {
+        $this->responseService = $responseService;
+    }
     public function find(int $id)
     {
 
--- app/Services/JsonResponseService.php
+++ app/Services/JsonResponseService.php
@@ -4,5 +4,14 @@ namespace App\Services;
 
 class JsonResponseService
 {
-
+    public function getFormat(string $message, mixed $data = false): array
+    {
+        $result = [
+            'message' => $message,
+        ];
+        if ($data) {
+            $result ['data'] = $data;
+        }
+        return $result;
+    }
 }
